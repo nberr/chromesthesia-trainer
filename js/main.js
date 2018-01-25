027ad9beb7aee6ce959c6cc1123ab5fbbd4f3ff5
nberriochoa <nberriochoa@gmail.com>
2018-01-25 14:44:59 -0800

    random colors and sound

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 document.writeln('Hello, world!');
 
-function chBackcolor(color) {
+function randomColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function randomSound() {
+
+}
+
+function playSound() {
+    var audio = new Audio('res/mp3/something.mp3');
+
+    audio.play();
+}
+
+function playSomething() {
+
+    setColor(randomColor());
+
+    playSound(randomSound());
+
+}
+
+function setColor(color) {
     document.body.style.background = color;
 }
