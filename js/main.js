e4ea00d37443fa864abe875f1d3d005e01015b3f
nberriochoa <nberriochoa@gmail.com>
2018-01-26 20:17:52 -0800

    removed audio files

--- js/main.js
+++ js/main.js
@@ -2,52 +2,55 @@ document.writeln('Hello, world!');
 
 var my_colors = {
     perfect_unison : 'red',
-    minor_second : 'green',
-    major_second : 'blue',
-    minor_third : 'beige',
-    major_third : 'coral',
-    perfect_fourth : 'yellow',
-    augmented_fourth : 'brown',
-    perfect_fifth : 'cyan',
-    minor_sixth : 'violet',
-    major_sixth : 'white',
-    minor_seventh : 'black',
-    major_seventh : 'gold',
-    perfect_octave : 'silver'
+    minor_second : 'orangered',
+    major_second : 'orange',
+    minor_third : 'tangerine',
+    major_third : 'yellow',
+    perfect_fourth : 'yellowgreen',
+    augmented_fourth : 'green',
+    perfect_fifth : '#7BCCB5', /* blue green */
+    minor_sixth : '#blue',
+    major_sixth : 'blueviolet',
+    minor_seventh : 'violet',
+    major_seventh : 'mediumvioletred',
+    perfect_octave : 'red'
 };
 
 var intervals = ["perfect_unison", "minor_second", "major_second", "minor_third", "major_third", "perfect_fourth",
                  "augmented_fourth", "perfect_fifth", "minor_sixth", "major_sixth", "minor_seventh", "major_seventh",
                  "perfect_octave"];
 
+var previous = "perfect_unison";
+
+/* var timer = Date.now();*/
+
 function randomInterval() {
     return intervals[Math.floor(Math.random() * intervals.length)];
 }
 
-function randomColor() {
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
-}
-
-function randomSound() {
-
-}
-
+/* play the interval */
 function playSound(interval) {
     var audio = new Audio('res/audio/'.concat(interval, '/tmp.mp3'));
     audio.play();
 }
 
+/* this function gets called on a button press */
 function playSomething() {
 
     // get a random interval
     var interval = randomInterval();
 
+    while (interval === previous) {
+        interval = randomInterval();
+    }
+    previous = interval;
 
     setColor(my_colors[interval]);
 
-    playSound(interval);
+    /* playSound(interval); */
 }
 
+/* set the background color */
 function setColor(color) {
     document.body.style.background = color;
 }
