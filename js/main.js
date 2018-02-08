28349b2fa81c11689e7a9cb4431cf5f408f70772
nberriochoa <nberriochoa@gmail.com>
2018-02-08 15:32:24 -0800

    each mode has own column

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-/* hard coded values */
+/* hard coded color values */
 var my_colors = {
     perfect_unison : '#c0c0c0',
     minor_second   : '#000000',
@@ -16,20 +16,34 @@ var my_colors = {
     perfect_octave : '#900090'
 };
 
+/* intervals allowed */
 var intervals = ["perfect_unison", "minor_second", "major_second", "minor_third", "major_third", "perfect_fourth",
                  "tritone", "perfect_fifth", "minor_sixth", "major_sixth", "minor_seventh", "major_seventh",
                  "perfect_octave"];
 
+/*
+ * root of the interval
+ * V1: only use c3 for the root
+ */
 var roots = ['c3', 'c#3', 'd3', 'd#3', 'e3', 'f3', 'f#3','g3', 'g#3', 'a3', 'a#3', 'b3',
     'c4', 'c#4', 'd4', 'd#4', 'e4', 'f4', 'f#4','g4', 'g#4', 'a4', 'a#4', 'b4',
     'c5'];
 
 
+
+
+
+
+
+
+
+
 var audio = null;
 
 var previous = null;
+var answer = null;
 
-/* var timer = Date.now();*/
+var timer = Date.now();
 
 function randomRoot() {
     return roots[Math.floor(Math.random() * roots.length)];
@@ -43,6 +57,25 @@ function randomSound() {
 
 }
 
+function start_test() {
+    /* generate a random interval */
+    playSomething();
+
+    /* start the timer */
+    timer = Date.now();
+
+    /* wait for the user to answer */
+    while (answer === null) {
+        if (answer === previous) {
+            /* correct answer; stop the timer */
+
+        }
+    }
+}
+
+function answer(interval) {
+    answer = interval;
+}
 
 /*
  * get the path to an interval
